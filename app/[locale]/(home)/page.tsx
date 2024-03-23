46bda94dc798efba7aed869df651dd5f3630d599
YoussefMohamedSalah <edge.mohamed120@gmail.com>
2024-03-23 06:36:31 +0200

    solving bugs

--- app/[locale]/(home)/page.tsx
+++ app/[locale]/(home)/page.tsx
@@ -1,13 +1,3 @@
-import HeroSection from "./hero-section";
-import SecondSection from "./second-section";
-import ThirdSection from "./third-section";
-import FourthSection from "./fourth-section";
-import FifthSection from "./fifth-section";
-import SixthSection from "./sixth-section";
-import SeventhSection from "./seventh-section";
-import GetStartedFree from "./eighth-section";
-import AboutUsSection from "./about-us-section";
-import MainCarousel from "../../../components/home/MainCarousel";
 import HomeWrapper from "@/components/home/HomeWrapper";
 
 interface Props {

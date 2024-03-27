35ec65b0e342c0f45c43f48307b6aa7b977c1572
YoussefMohamedSalah <edge.mohamed120@gmail.com>
2024-03-27 20:16:15 +0200

    .

--- components/header/TopHeader.tsx
+++ components/header/TopHeader.tsx
@@ -9,51 +9,37 @@ interface Props {
 }
 
 const TopHeader = ({ data }: Props) => {
-
-    const navbarClasses = `
-    flex items-center justify-between space-x-10 h-14
-    sticky top-0 z-50 border-b border-gray-600 bg-black`;
-
     return (
-        <div className={navbarClasses}>
-            <div className="container flex justify-between items-center">
-                <ul className="md:flex flex items-center justify-center gap-2">
+        <div className="bg-black md:sticky top-0 z-50 border-b border-gray-600">
+            <div className="container flex flex-col md:flex-row justify-between items-center p-4">
+                <div className="flex flex-col md:flex-row items-center justify-center md:gap-2">
                     {data.ar_address && (
-                        <div className="flex items-center">
+                        <div className="flex items-center mb-2 md:mb-0">
                             <IoLocationSharp className="text-2xl me-1 text-yellow-500" />
-                            <span className="ml-1">
-                                <span>{data.ar_address}</span>
-                            </span>
+                            <span className="text-center">{data.ar_address}</span>
                         </div>
                     )}
-                    <div className="font-thin md:flex flex items-center hidden ml-1">|</div>
                     {data.phone_number_1 && (
-                        <li className="flex items-center">
-                            <LiaMobileAltSolid className="text-2xl me-1 text-yellow-500" />
-                            <span className="ml-1">
-                                <a href={`tel:${data.phone_number_1}`}>{data.phone_number_1}</a>
-                            </span>
-                        </li>
-                    )}                </ul>
-                <ul className="flex items-between justify-center gap-2">
-                    {data.email && (
                         <div className="flex items-center">
-                            <MdAlternateEmail className="text-2xl me-1 text-yellow-500" />
-                            <span className="ml-1">
-                                <a href={`mailto:${data.email}`}>{data.email}</a>
-                            </span>
+                            <LiaMobileAltSolid className="text-2xl me-1 text-yellow-500" />
+                            <a href={`tel:${data.phone_number_1}`}>{data.phone_number_1}</a>
                         </div>
                     )}
-                    <div className="font-thin md:flex flex items-center hidden ml-1">|</div>
-                    {data.land_line && (
-                        <li className="flex items-center">
-                            <GiRotaryPhone className="text-2xl me-1 text-yellow-500" />
-                            <span className="ml-1">
-                                <a href={`tel:${data.land_line}`}>{data.land_line}</a>
-                            </span>
-                        </li>
+                </div>
+                <div className="flex flex-col md:flex-row items-center justify-center md:gap-2">
+                    {data.email && (
+                        <div className="flex items-center mb-2 md:mb-0">
+                            <MdAlternateEmail className="text-2xl me-1 text-yellow-500" />
+                            <a href={`mailto:${data.email}`}>{data.email}</a>
+                        </div>
                     )}
-                </ul>
+                    {data.land_line && (
+                        <div className="flex items-center">
+                            <GiRotaryPhone className="text-2xl me-1 text-yellow-500" />
+                            <a href={`tel:${data.land_line}`}>{data.land_line}</a>
+                        </div>
+                    )}
+                </div>
             </div>
         </div>
     )
